1fc49e07afb0384c1fe0df7abd6936e05267241a
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-09-05 22:35:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/challengeform" element={<ChallengeForm />} />
-          <Route path="/challengedetail" element={<ChallengeDetail />} />
-          <Route path="/editchallenge/:id" element={<EditChallenge />} />
+          <Route path="https://hachthon-dashboard.vercel.app/challengeform" element={<ChallengeForm />} />
+          <Route path="https://hachthon-dashboard.vercel.app/challengedetail" element={<ChallengeDetail />} />
+          <Route path="https://hachthon-dashboard.vercel.app/editchallenge/:id" element={<EditChallenge />} />
         </Routes>
       </BrowserRouter>
     </EventProvider>
